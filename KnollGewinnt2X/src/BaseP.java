d4d35ed676ede1f7b2b5f126d8a8e31a19cdd645
2236367 <2236367@EZM23091.ez.edeka.net>
2018-06-01 19:20:34 +0200

    .

--- KnollGewinnt2X/src/BaseP.java
+++ KnollGewinnt2X/src/BaseP.java
@@ -16,6 +16,8 @@ import javax.swing.JPanel;
 
 public class BaseP extends JPanel {
 
+	private static final int left = 1;
+	private static final int right = 2;
 	private PlayPanel[][] playBoard;
 	private GroundPanel[] control;
 	ArrayList<StockPanel> stocks;
@@ -88,22 +90,31 @@ public class BaseP extends JPanel {
 
 	}
 
+	/**
+	 * 
+	 * @param direction
+	 *            - information about where to move the pointer
+	 */
 	public void changePointer(int direction) {
 		int i = getActiveGroundPanel();
 		switch (direction) {
-		// 1 steht für links und 2 steht für rechts.
-		case 1:
+
+		case left:
 
 			if (i > 0 && i < (control.length)) {
+				// ---deactivate current pointer---
 				control[i].setPointer(false, player);
+				// ---activate pointer to the left
 				control[i - 1].setPointer(true, player);
 			}
 			return;
 
-		case 2:
+		case right:
 
 			if (i >= 0 && i < (control.length) - 1) {
+				// ---deactivate current pointer---
 				control[i].setPointer(false, player);
+				// ---activate pointer to the right
 				control[i + 1].setPointer(true, player);
 			}
 			return;
@@ -113,22 +124,32 @@ public class BaseP extends JPanel {
 		}
 	}
 
+	/**
+	 * Throws the coin of the current Player into the coloumn which is selected by
+	 * the pointer
+	 * 
+	 * @param player
+	 *            - the player who throws the coin
+	 */
 	public void throwCoin(int player) {
-		for (int i = 0; i < control.length; i++) {
-			if (control[i].pointer == true) {
-				for (int j = playBoard.length - 1; j >= 0; j--) {
-					if (playBoard[j][i].isFilled == false) {
-						playBoard[j][i].fill(player);
-						return;
-					}
 
-				}
+		for (int j = playBoard.length - 1; j >= 0; j--) {
+
+			// ---fill next free playPanel starting from the bottom
+			if (playBoard[j][getActiveGroundPanel()].isFilled == false) {
+				playBoard[j][getActiveGroundPanel()].fill(player);
 				return;
 			}
+
 		}
+		return;
 
 	}
 
+	/**
+	 * 
+	 * @return - returns the index of the selected GroundPanel by the pointer
+	 */
 	private int getActiveGroundPanel() {
 		for (int i = 0; i < control.length; i++) {
 			if (control[i].pointer == true) {
@@ -138,14 +159,17 @@ public class BaseP extends JPanel {
 		return -1;
 	}
 
+	/**
+	 * changes the pointer to the current player (calls setPointer who changes the color)
+	 */
 	public void changePlayer() {
 		control[getActiveGroundPanel()].setPointer(true, player);
 	}
 
-	public void newGame() {
-
-	}
-
+	/**
+	 * analyzes the current playBoard and generates forms of 4 playpanels (1 stockpanel contains 4 playpanels).
+	 * @param stocks - contains a list of all possible combinations of 4 playpanels
+	 */
 	public void generateStocks() {
 		stocks = new ArrayList<StockPanel>();
 		for (int i = 0; i < playBoard.length; i++) {
@@ -160,12 +184,12 @@ public class BaseP extends JPanel {
 					stocks.add(new StockPanel(playBoard[i][j], playBoard[i][j + 1], playBoard[i][j + 2],
 							playBoard[i][j + 3]));
 				}
-				// --diagonale Stacks #1---
+				// --diagonale Stocks #1---
 				if (j + 3 < playBoard[i].length && i + 3 < playBoard.length) {
 					stocks.add(new StockPanel(playBoard[i][j], playBoard[i + 1][j + 1], playBoard[i + 2][j + 2],
 							playBoard[i + 3][j + 3]));
 				}
-				// --diagonale Stacks #2---
+				// --diagonale Stocks #2---
 				if (j - 3 < playBoard[i].length && i - 3 < playBoard.length && j - 3 >= 0 && i - 3 >= 0) {
 					stocks.add(new StockPanel(playBoard[i][j], playBoard[i - 1][j - 1], playBoard[i - 2][j - 2],
 							playBoard[i - 3][j - 3]));
