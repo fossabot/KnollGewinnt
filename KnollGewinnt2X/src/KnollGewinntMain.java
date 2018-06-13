849055f1d6fadf97b4afa6e4900a06dd76bf0831
paul <paul@141.72.137.193>
2018-06-13 14:24:46 +0200

    fix2

--- KnollGewinnt2X/src/KnollGewinntMain.java
+++ KnollGewinnt2X/src/KnollGewinntMain.java
@@ -4,7 +4,7 @@ import java.io.IOException;
  * KNOLL GEWINNT powered by javax.swing
  * CLASS: KnollGewinntMain
  * @since 29.05.2018
- * @author Caspar Goldmann, Elias Klewar, Moritz Cabral, Timo Büchert, Paul Schwarz
+ * @author Caspar Goldmann, Elias Klewar, Moritz Cabral, Timo Büchert, Paul Schwarzz
  * @version 0.1
  * (c) 2018
  */
