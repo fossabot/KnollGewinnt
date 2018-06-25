5035bc8ee0fe38d0028024224805ad8cd8f23bac
2236367 <2236367@EZM23091.ez.edeka.net>
2018-06-25 20:04:54 +0200

    KnollPlayer auskommentiert

--- KnollGewinnt2X/src/gamePackage/KnollPlayer.java
+++ KnollGewinnt2X/src/gamePackage/KnollPlayer.java
@@ -1,3 +1,11 @@
+/** 
+ * KNOLL GEWINNT powered by javax.swing
+ * CLASS: MainFrame
+ * @since 29.05.2018
+ * @author Caspar Goldmann, Elias Klewar, Moritz Cabral, Timo Büchert, Paul Schwarz
+ * @version 0.1
+ * (c) 2018
+ */
 package gamePackage;
 
 import java.io.BufferedReader;
@@ -9,11 +17,21 @@ import java.util.List;
 
 public class KnollPlayer {
 
+	
+	
 	private String name;
 	private int playedGames;
 	private int wins;
 	private int stepsToWin;
 
+	
+	/**
+	 * Contructor of Class KnollPlayer
+	 * @param name - Players nickname
+	 * @param playedGames - Amount of Played Games by the Player
+	 * @param wins	- Amount of Wins by the Player
+	 * @param stepsToWin - Minimum steps to win ever reached
+	 */
 	public KnollPlayer(String name, int playedGames, int wins, int stepsToWin) {
 		this.name = name;
 		this.playedGames = playedGames;
@@ -21,10 +39,17 @@ public class KnollPlayer {
 		this.stepsToWin = stepsToWin;
 	}
 
+	/**
+	 * Play a new Game
+	 */
 	public void playGame() {
 		playedGames++;
 	}
 
+	/**
+	 * Increase Wins and take stepsToWin if theyre smaller than before
+	 * @param stepsToWin
+	 */
 	public void setWin(int stepsToWin) {
 		wins++;
 		if (stepsToWin < this.stepsToWin)
@@ -32,10 +57,6 @@ public class KnollPlayer {
 
 	}
 	
-	public void play() {
-		this.playedGames++;
-	}
-
 	/**
 	 * @return the name
 	 */
